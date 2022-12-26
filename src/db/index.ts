69e9cb13344bce6ce510b9d58bce59775a18f779
a.lamtev <a.lamtev@computer5.local>
2022-12-26 13:19:52 +0300

    add env postgres port

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const dataBase = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST || "localhost",
-  port: 9991,
+  port: +process.env.POSTGRES_PORT! || 9991,
   username: process.env.POSTGRES_USER || "postgres",
   password: process.env.POSTGRES_PASSWORD || "postgres",
   database: process.env.POSTGRES_BASE_NAME || "referals",
